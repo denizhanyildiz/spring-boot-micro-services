a8abf7c52c2eede84693a7b6ac7a480323ca4c53
denizhanyildiz <denizhan_yildiz@hotmail.com>
2024-07-19 22:49:58 +0300

    organization service documentation done.

--- organization-service/src/main/java/com/javaguides/organizationservice/OrganizationServiceApplication.java
+++ organization-service/src/main/java/com/javaguides/organizationservice/OrganizationServiceApplication.java
@@ -1,8 +1,32 @@
 package com.javaguides.organizationservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Organization Service REST APIs",
+				description = "Organization Service REST APIs Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Denizhan",
+						email = "denizhan_yildiz@hotmail.com",
+						url = "https://www.linkedin.com/in/denizhan-yildiz/"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://github.com/denizhanyildiz"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Department-Service Doc",
+				url = "https://github.com/denizhanyildiz"
+		)
+)
 @SpringBootApplication
 public class OrganizationServiceApplication {
 
--- organization-service/src/main/java/com/javaguides/organizationservice/controller/OrganizationController.java
+++ organization-service/src/main/java/com/javaguides/organizationservice/controller/OrganizationController.java
@@ -2,11 +2,18 @@ package com.javaguides.organizationservice.controller;
 
 import com.javaguides.organizationservice.dto.OrganizationDto;
 import com.javaguides.organizationservice.service.OrganizationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "Organization Service - Organization Controller",
+        description = "Organization Controller Exposes REST APIs for Organization-Service"
+)
 @RestController
 @RequestMapping("api/organizations")
 @AllArgsConstructor
@@ -15,12 +22,28 @@ public class OrganizationController {
 
     private final OrganizationService organizationService;
 
+    @Operation(
+            summary = "Save organization REST API",
+            description = "Save organization REST API is used save organization object in database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http status 201 CREATED"
+    )
     @PostMapping
     public ResponseEntity<OrganizationDto> saveOrganization(@RequestBody OrganizationDto organizationDto) {
         OrganizationDto savedOrganizationDto = organizationService.saveOrganization(organizationDto);
         return new ResponseEntity<>(savedOrganizationDto, HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Get organization REST API",
+            description = "Get organization REST API is used get organization object from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http status 200 SUCCESS"
+    )
     @GetMapping("/{organizationCode}")
     public ResponseEntity<OrganizationDto> getOrganizationByCode(@PathVariable("organizationCode") String organizationCode) {
         OrganizationDto organizationDto = organizationService.getOrganizationByCode(organizationCode);
